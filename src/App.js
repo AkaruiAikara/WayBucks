4b979f405d0f242e5e6df672555d8a31daea9020
--get <vall@aikara.me>
2022-02-02 21:58:41 +0700

    components: Add Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Jumbotron />
     </div>
   )
 }
